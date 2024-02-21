15bb3b899f8b687fc2f615691901d7a94f12b41a
jacty <saej3101@usherbrooke.ca>
2024-02-21 09:21:29 -0500

    Added labo 1

--- code/helpers/analysis.py
+++ code/helpers/analysis.py
@@ -243,10 +243,16 @@ def project_onto_new_basis(data, basis):
     dims = np.asarray(data).shape
     assert dims[-1] == len(basis)
     projected = np.zeros(np.asarray(data).shape)
+
+    # projected = np.dot(data, basis)
+    # projected = np.matmul(data, basis)
     for i in range(dims[0]):  # dims[0] = n_classes
         tempdata = data[i]
         # TODO Labo L1.E2.5 Remplacer l'opération bidon par la bonne projection ici
         # projected[i] = ??
+        projected[i] = np.matmul(tempdata, basis)
+
+    # print(f'Projected data: {projected}')     
     return projected
 
 
